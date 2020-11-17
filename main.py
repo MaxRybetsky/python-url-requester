8a0b318eafcf89cb7e351c936b4638cf8d0e86ce
MaxRybetsky <stsdewar@ya.ru>
2020-11-17 16:16:07 +0300

    Add file main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def req_runner(list_of_urls, number_of_requests):
+    for i in range(len(list_of_urls)):
+        print("Request to " + list_urls[i])
+        for j in range(number_of_requests):
+            response = requests.get(list_of_urls[i])
+            print(response.status_code)
+            # print(response.text)
+            print()
+        print("End of requesting to " + list_urls[i])
+
+
+list_urls = [
+    "https://www.avito.ru/",
+    "https://www.w3schools.com/",
+    "https://www.datacamp.com/",
+    "https://skillbox.ru/",
+    "https://yandex.ru/",
+    "https://mipt.ru/",
+    "https://mephi.ru/",
+    "https://tproger.ru/",
+    "http://spec-zone.ru/"
+]
+
+req_runner(list_urls, 5)
